1461ae88dcc4bb77306dd3e286bcce0c2c19e708
esnya <2088693+esnya@users.noreply.github.com>
2024-12-07 16:58:28 +0900

    :tada: feat!: Initial Commit for Resonite

---
+++ LODTweaks.cs
@@ -0,0 +1,78 @@
+﻿using FrooxEngine;
+using FrooxEngine.UIX;
+using HarmonyLib;
+using ResoniteModLoader;
+using System;
+using System.Reflection;
+
+namespace LODTweaksMod
+{
+    public class LODTweaks : ResoniteMod
+    {
+        public override string Name => "LODTweaks";
+        public override string Author => "esnya";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/esnya/ResoniteLODTweaks";
+
+        public override void OnEngineInit()
+        {
+            var harmony = new Harmony("com.nekometer.esnya.LODTweaks");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch]
+        class LODGroupPatch
+        {
+            static MethodBase TargetMethod()
+            {
+                return typeof(LODGroup).GetMethod("OnInit", BindingFlags.NonPublic | BindingFlags.Instance);
+            }
+
+            static void Postfix(object __instance)
+            {
+                if (__instance is LODGroup lodGroup)
+                {
+                    lodGroup.UpdateOrder = 1000;
+                }
+            }
+        }
+
+        [HarmonyPriority(HarmonyLib.Priority.LowerThanNormal)]
+        [HarmonyPatch(typeof(WorkerInspector), nameof(WorkerInspector.BuildInspectorUI))]
+        class WorkerInspectorPatch
+        {
+            static void Postfix(WorkerInspector __instance, Worker worker, UIBuilder ui)
+            {
+                if (worker is LODGroup lodGroup)
+                {
+                    BuildInspectorUI(lodGroup, ui);
+                }
+            }
+
+            private static void BuildInspectorUI(LODGroup lodGroup, UIBuilder ui)
+            {
+                Button(ui, "[Mod] Add LOD Level from children", () => SetupFromChildren(lodGroup));
+                Button(ui, "[Mod] Remove LODGroups from children", () => RemoveFromChildren(lodGroup));
+            }
+
+            private static void Button(UIBuilder ui, string text, System.Action onClick)
+            {
+                var button = ui.Button(text);
+                button.IsPressed.OnValueChange += (value) =>
+                {
+                    if (value) onClick();
+                };
+            }
+
+            private static void SetupFromChildren(LODGroup lodGroup)
+            {
+                lodGroup.AddLOD(0.01f, lodGroup.Slot);
+            }
+
+            private static void RemoveFromChildren(LODGroup lodGroup)
+            {
+                lodGroup.Slot.GetComponentsInChildren<LODGroup>(c => c != lodGroup).ForEach(c => c.Destroy());
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LODTweaks")]
+[assembly: AssemblyDescription("LOD Tweaks for FrooxEngine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("esnya")]
+[assembly: AssemblyProduct("LODTweaks")]
+[assembly: AssemblyCopyright("Copyright © esnya 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("bbe5cf58-5e19-44bf-b757-530ee71182c6")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
